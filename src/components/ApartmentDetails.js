7b6b23a39b0368b179c47946035206dc87f29df3
Filipe Ferreira <filipeferreira94@gmail.com>
2023-06-23 15:03:02 +0100

    Apartment details is bugged. AxiosError

--- src/components/ApartmentDetails.js
+++ src/components/ApartmentDetails.js
@@ -1,3 +1,38 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 export function ApartmentsDetails() {
- 
+	const id = useParams();
+	const [apartment, setApartment] = useState([]);
+
+	useEffect(() => {
+		const getApartmentById = async (id) => {
+			try {
+				const response = await axios.get(
+					process.env.REACT_APP_API_URL + `/apartments/${id}`
+				);
+				console.log(response);
+				setApartment(response.data);
+			} catch (error) {
+				console.log('error: ', error);
+			}
+		};
+		getApartmentById(id);
+	}, []);
+
+	return (
+		<div className="container d-flex justify-content-center">
+			<Card className="m-4" style={{ width: '18rem' }}>
+				<Card.Img variant="top" src={apartment.img} />
+				<Card.Body>
+					<Card.Title>{apartment.title}</Card.Title>
+					<Card.Text>{apartment.description}</Card.Text>
+				</Card.Body>
+			</Card>
+		</div>
+	);
 }
