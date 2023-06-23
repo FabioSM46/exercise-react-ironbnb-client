373156f81216f4a10528390c58ee438d1a3191f1
Filipe Ferreira <filipeferreira94@gmail.com>
2023-06-23 13:22:49 +0100

    1 done - routing setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import ironhackersImg from "./assets/ironhackers.avif"
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import { NavBar } from './components/NavBar';
+import { HomePage } from './components/HomePage';
+import { ApartmentsDetails } from './components/ApartmentDetails';
+import { ApartmentsList } from './components/ApartmentsList';
+import { CreateApartment } from './components/CreateApartment';
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Welcome</h1>
-      <img src={ironhackersImg} alt="ironhackers" />
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Welcome</h1>
+
+			<NavBar />
+
+			<Routes>
+				<Route path="/" element={<HomePage />}></Route>
+				<Route path="/apartments" element={<ApartmentsList />} />
+				<Route path="/apartments/:id" element={<ApartmentsDetails />} />
+				<Route path="/apartments/create" element={<CreateApartment />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
