d36d66c9d99916558f2f922bd59eb574b36634b9
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-06-23 14:44:10 +0200

    create apartment

--- src/components/CreateApartment.js
+++ src/components/CreateApartment.js
@@ -1,1 +1,46 @@
-export function CreateApartment() {}
+export function CreateApartment() {
+  const handleSubmit = () => {};
+
+  return (
+    <form className="container CreateApartment" onSubmit={handleSubmit}>
+      <label>
+        Title:
+        <input
+          type="text"
+          name="title"
+          placeholder="enter the title"
+          value=""
+          required={true}
+          onChange={() => {}}
+        />
+      </label>
+
+      <label>
+        Price per Day:
+        <input
+          type="number"
+          name="price"
+          placeholder="enter the price per day"
+          value=""
+          required={true}
+          onChange={() => {}}
+        />
+      </label>
+
+      <label>
+        Image:
+        <input
+          type="text"
+          name="image"
+          placeholder="enter image link"
+          value=""
+          required={true}
+          onChange={() => {}}
+        />
+      </label>
+      <button className="btn" type="submit">
+        Create
+      </button>
+    </form>
+  );
+}
