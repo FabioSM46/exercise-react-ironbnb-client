b2dbcd8dc12715a5131b095d26679ce577878771
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-06-23 15:26:36 +0200

    create ap

--- src/components/CreateApartment.js
+++ src/components/CreateApartment.js
@@ -1,17 +1,40 @@
+import axios from "axios";
+import { useState } from "react";
 export function CreateApartment() {
-  const handleSubmit = () => {};
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleSubmit = () => {
+    const newApartment = {
+      title: { title },
+      pricePerDay: { price },
+      img: { image },
+    };
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/apartments`, newApartment)
+      .then((res) => {})
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
   return (
-    <form className="container CreateApartment" onSubmit={handleSubmit}>
+    <form
+      className="container CreateApartment d-flex flex-column m-2"
+      onSubmit={handleSubmit}
+    >
       <label>
         Title:
         <input
           type="text"
           name="title"
           placeholder="enter the title"
-          value=""
+          value={title}
           required={true}
-          onChange={() => {}}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
         />
       </label>
 
@@ -21,24 +44,27 @@ export function CreateApartment() {
           type="number"
           name="price"
           placeholder="enter the price per day"
-          value=""
+          value={price}
           required={true}
-          onChange={() => {}}
+          onChange={(e) => {
+            setPrice(e.target.value);
+          }}
         />
       </label>
 
       <label>
         Image:
         <input
-          type="text"
+          type="url"
           name="image"
           placeholder="enter image link"
-          value=""
-          required={true}
-          onChange={() => {}}
+          value={image}
+          onChange={(e) => {
+            setImage(e.target.value);
+          }}
         />
       </label>
-      <button className="btn" type="submit">
+      <button className="btn btn-primary w-25" type="submit">
         Create
       </button>
     </form>
