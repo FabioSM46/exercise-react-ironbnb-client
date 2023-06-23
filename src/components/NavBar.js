1bef285d3a033177137e22d1820277c4d082d738
Filipe Ferreira <filipeferreira94@gmail.com>
2023-06-23 13:41:55 +0100

    Added Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,1 +1,24 @@
-export function NavBar() {}
+import { NavLink } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
+export function NavBar() {
+	return (
+		<>
+			<NavLink to={'/'}>
+				<Button className="m-2" variant="primary">
+					Home
+				</Button>
+			</NavLink>
+			<NavLink to={'/apartments'}>
+				<Button className="m-2" variant="primary">
+					Apartment List
+				</Button>
+			</NavLink>
+			<NavLink to={'/apartments/create'}>
+				<Button className="m-2" variant="primary">
+					Create Apartment
+				</Button>
+			</NavLink>
+		</>
+	);
+}
